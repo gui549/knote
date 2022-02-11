b0fc0d5395dfbe29f7f57932b01b0a8e5eabee6c
Byeongmin Kong <gui549@naver.com>
2022-02-11 15:43:55 +0900

    Use replica set of mongoDB

--- src/main/java/learnk8s/io/knotejava/KnoteConfig.java
+++ src/main/java/learnk8s/io/knotejava/KnoteConfig.java
@@ -1,27 +1,23 @@
-package learnk8s.io.knotejava;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-
-@EnableWebMvc
-@Configuration
-public class KnoteConfig implements WebMvcConfigurer {
-
-    @Autowired
-    private KnoteProperties properties;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        System.out.println("Resource Handler Called");
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + properties.getUploadDir())
-                .setCachePeriod(3600)
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver());
-    }
-}
+//package learnk8s.io.knotejava;
+//
+//import com.mongodb.ReadPreference;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@EnableWebMvc
+//@Configuration
+//public class KnoteConfig implements WebMvcConfigurer {
+//
+//    private final MongoTemplate mongoTemplate;
+//
+//    @Autowired
+//    public KnoteConfig(MongoTemplate mongoTemplate) {
+//        this.mongoTemplate = mongoTemplate;
+//        mongoTemplate.setReadPreference(ReadPreference.secondary());
+//    }
+//
+//
+//}
